cc5bd68ddc1d81d1923996cced2a97e0afd94e63
Promomotionbot <86830116+Promomotionbot@users.noreply.github.com>
2021-08-06 08:05:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,68 +2,20 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+Start click ചെയ്തിട്ടും കാര്യമില്ല.എന്റെ വിധി എന്റെ [അഡ്മിന്റെ](https://t.me/nokiyirunno_ippo_kittum) വാക്കുകൾ ആണ്.
+ഞാൻ @MovieArcade ൽ മാത്രമേ പണി എടുക്കുകയുള്ളൂ.
+"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
+    HELP_MSG = """ ഞാൻ പറഞ്ഞില്ലേ എന്നെ @MovieArcade ൽ മാത്രമേ use ചെയ്യാൻ പറ്റുകയുള്ളു എന്ന്.... പിന്നെ എന്തിനാ വീണ്ടും ശശി ആകുന്നത്  😂"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> [Owner](https://t.me/Jinn_007)
 
-⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Language :</b> <code>Malayalam </code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Library :</b> School library </a>
 
 """
